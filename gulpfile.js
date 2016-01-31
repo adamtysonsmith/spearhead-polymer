d872cef258a3cb38a7dd8e86b6edcd2abf7b1e96
adamtysonsmith <atsmith227@gmail.com>
2016-01-31 14:31:16 -0700

    restructured polymer element imports

--- gulpfile.js
+++ gulpfile.js
@@ -7,63 +7,58 @@ var rename = require('gulp-rename');
 
 var paths = {
   headScripts: [
-    './bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    './public/bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
   bodyScripts: [
     './public/js/*.js'
-  ],
-  polymerLib: [
-    './bower_components/polymer/polymer.html',
-    './bower_components/polymer/polymer-mini.html',
-    './bower_components/polymer/polymer-micro.html'
   ]
 }
 
-// Clean up old files hanging around
+// Clean up before build
 gulp.task('clean', function() {
   return del(['./public/build']);
 });
 
 // Main Styles and Scripts
 gulp.task('main-styles', function() {
-  gulp.src('./public/sass/main.scss')
-  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-  .pipe(concat('styles.css'))
-  .pipe(gulp.dest('./public/build'));
+  return gulp.src('./public/sass/main.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('./public/build'));
 });
 
 gulp.task('head-scripts', function() {
-  gulp.src(paths.headScripts)
-  .pipe(concat('head-scripts.js'))
-  .pipe(gulp.dest('./public/build'));
+  return gulp.src(paths.headScripts)
+    .pipe(concat('head-scripts.js'))
+    .pipe(gulp.dest('./public/build'));
 });
 
 gulp.task('body-scripts', function() {
-  gulp.src(paths.bodyScripts)
-  .pipe(concat('body-scripts.js'))
-  .pipe(gulp.dest('./public/build'));
+  return gulp.src(paths.bodyScripts)
+    .pipe(concat('body-scripts.js'))
+    .pipe(gulp.dest('./public/build'));
 });
 
 // Polymer Stuff
-gulp.task('polymer', function() {
-  gulp.src(paths.polymerLib)
-  .pipe(gulp.dest('./public/build'));
+gulp.task('polymer-imports', function() {
+  return gulp.src('./elements/imports.html')
+    .pipe(gulp.dest('./public/build'));
 });
 
 gulp.task('polymer-components', function() {
-  gulp.src('./elements/*/*.jade')
-  .pipe(jade())
-  .pipe(concat('elements.html'))
-  .pipe(gulp.dest('./public/build'));
+  return gulp.src('./elements/*/*.jade')
+    .pipe(jade())
+    .pipe(concat('elements.html'))
+    .pipe(gulp.dest('./public/build'));
 });
 
 gulp.task('component-styles', function() {
-  gulp.src('./elements/*/*.scss')
-  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-  .pipe(rename(function(path) {
-    return path;
-  }))
-  .pipe(gulp.dest('./elements'));
+  return gulp.src('./elements/*/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename(function(path) {
+      return path;
+    }))
+    .pipe(gulp.dest('./elements'));
 });
 
 gulp.task('default', [
@@ -71,7 +66,7 @@ gulp.task('default', [
   'main-styles',
   'head-scripts',
   'body-scripts',
-  'polymer',
+  'polymer-imports',
   'polymer-components',
   'component-styles'
 ]);
