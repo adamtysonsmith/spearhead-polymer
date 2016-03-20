cd528700e76c782032fc009f777a7e42a908ff73
adamtysonsmith <atsmith227@gmail.com>
2016-03-20 17:55:54 -0600

    date selection updates chart

--- elements/projects-waterfall/waterfall.js
+++ elements/projects-waterfall/waterfall.js
@@ -2,17 +2,51 @@
 
 Polymer({
   is: 'projects-waterfall',
+  properties: {
+    start: {
+      type: String,
+      notify: true,
+      value: todaysDate
+    },
+    end: {
+      type: String,
+      notify: true,
+      value: twoMonthsFromNow
+    },
+    data: {
+      type: Array,
+      value: getData
+    }
+  },
   getData: getData,
   configureVars: configureVars,
   drawChart: drawChart,
+  nukeChart: nukeChart,
   ready: function() {
-    this.getData()
-    this.configureVars()
-    this.drawChart()
+    let defaultStart = this.start;
+    let defaultEnd   = this.end;
+    let defaultData  = this.data;
+    
+    this.addEventListener('start-changed', dateChanged);
+    this.addEventListener('end-changed', dateChanged);
+    
+    this.configureVars(defaultStart, defaultEnd, defaultData);
+    this.drawChart();
   }
 });
 
 
+// Event Listeners
+function dateChanged() {
+  let newStart = this.start;
+  let newEnd   = this.end;
+
+  this.nukeChart();
+  this.configureVars(newStart, newEnd, this.data);
+  this.drawChart();
+}
+
+// Get Data from API Routes
 function getData() {
   // @todo
   // Hit a route to retreive all the projects
@@ -36,19 +70,14 @@ function getData() {
     }
   ];
   
-  // Set the data on our instance
-  this.data = mockData;
+  return mockData;
 }
 
 
-
-function configureVars() {
-  // Set our required instance variables
-  // Maybe these should be passed in, data esp
-  const svgElement = this.$.waterfall;
-  const data = this.data;
+// D3 Config Vars
+function configureVars(startDate, endDate, data) {
+  const svgElement  = this.$.waterfall;
   
-  // D3 Layout variables
   const width       = this.width       = 1200;
   const height      = this.height      = 400;
   const sidePadding = this.sidePadding = 0;
@@ -65,16 +94,10 @@ function configureVars() {
   // Format the dates and times
   const dateFormat = this.dateFormat = d3.time.format('%Y-%m-%d');
   const timeFormat = this.timeFormat = d3.time.format('%b %e, %Y');
-  
-  // We trim off the current day and append the first or last day of the month roughly
-//  this.startMonth = start.slice(0, 8) + '01';
-//  this.endMonth = end.slice(0, 8) + '31';
-  const startMonth = this.startMonth = '2016-01-01'
-  const endMonth   = this.endMonth   = '2016-03-31';
 
   // min is the start selection, max is the end selection
-  const min = this.min = dateFormat.parse(startMonth);
-  const max = this.max = dateFormat.parse(endMonth);
+  const min = this.min = dateFormat.parse(startDate);
+  const max = this.max = dateFormat.parse(endDate);
 
   // Pass the min and max dates, create our time scale
   this.timeScale = d3.time.scale().domain([min, max]).range([40, width - 40]);
@@ -86,8 +109,7 @@ function configureVars() {
       .interpolate(d3.interpolateHcl);
 }
 
-
-
+// D3 Draw Chart
 function drawChart() {
   // draw chart using our instance vars
   let svgContainer = this.svgContainer;
@@ -102,9 +124,45 @@ function drawChart() {
   let data         = this.data;
   
   if(startDate < endDate) {
+    // Could use dependency injection for these (./d3-waterfall.js)
     drawAxis(svgContainer, 0, 0, width, height, startDate, endDate, timeScale, timeFormat);
     drawBars(data, svgContainer, 50, 0, 0, 40, colorScale, timeScale, dateFormat, width, height);
   } else {
-    console.log('Error, start data must be less than end date');
+    console.log('Error, start date must be less than end date');
   }
+}
+
+
+// Remove the chart so we can redraw
+function nukeChart() {
+  let chart = this.$.waterfall;
+  while (chart.firstChild) {
+    chart.removeChild(chart.firstChild);
+  }
+}
+
+
+// Date Helpers
+function todaysDate() {
+  var date = new Date();
+  var dd    = date.getDate();
+  var mm    = date.getMonth() + 1;
+  var yyyy  = date.getFullYear();
+
+  if (dd < 10) dd = '0' + dd
+  if (mm < 10) mm = '0' + mm
+
+  return yyyy + '-' + mm + '-' + dd;
+}
+
+function twoMonthsFromNow() {
+  var date  = new Date();
+  var mm    = date.getMonth() + 3;
+  var today = todaysDate();
+  var split = today.split('-');
+  
+  if (mm < 10) mm = '0' + mm;
+  split[1] = mm;
+  
+  return split.join('-');
 }
